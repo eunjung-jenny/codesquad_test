c209aecb8007370f89c2689d4ec3b5c158c24f25
eunjung-jenny <jenny.it.kr@gmail.com>
2019-12-04 04:10:33 +0000

    Add game over condition and message

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
+const resultCount = {
+  "스트라이크": 0,
+  "볼": 0,
+  "안타": 0,
+  "아웃": 3
+};
+
 function gameStart(){
   console.log("신나는 야구 게임!");
   console.log("첫 번째 타자가 타석에 입장했습니다. \n");
 }
 
+function gameOver(){
+  console.log(`최종 안타수: ${resultCount.안타}`);
+  console.log("GAME OVER");
+};
+
 function main(){
   gameStart();
-  
+  while (resultCount["아웃"]<3){
+    
+  }
+  gameOver();
 }
 
 main();
