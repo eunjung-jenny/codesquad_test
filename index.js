4587c4698bb1faf13489770528dc73f07b9b335a
eunjung-jenny <jenny.it.kr@gmail.com>
2019-12-04 12:47:54 +0000

    Modify miscellaneous things to be pretty

--- index.js
+++ index.js
@@ -1,23 +1,7 @@
+let teams = [];
 
-const INPUT_GUIDE = "\n타자 정보는 \n'타자 이름, 타율'\n형식으로 입력해주세요.\n타율은 0.1 초과 0.5 미만의 값으로 소수 셋째자리까지 입력해주세요.\n"
-
-let teams = [
-  {name: "코드",
-  members: 
-    [{name: "윤지수", prob: 0.499},
-     {name: "김정", prob: 0.482}, {name: "박서준", prob: 0.421}, {name: "김정", prob: 0.482}, {name: "박서준", prob: 0.421}, {name: "김정", prob: 0.482}, {name: "박서준", prob: 0.421}, {name: "김정", prob: 0.482}, {name: "박서준", prob: 0.421}
-    ]
-  },
-  {name: "스쿼드",
-  members: 
-    [{name: "정호영", prob: 0.490},
-     {name: "피오", prob: 0.452}, {name: "공유", prob: 0.382}, {name: "김정", prob: 0.482}, {name: "박서준", prob: 0.421}, {name: "김정", prob: 0.482}, {name: "박서준", prob: 0.421}, {name: "김정", prob: 0.482}, {name: "박서준", prob: 0.421}
-    ]
-  }
-];
-
-function gameStart(){
-  console.log("신나는 야구시합\n1.데이터 입력\n2.데이터 출력\n");
+function startingMessage(){
+  console.log("신나는 야구시합\n1.데이터 입력\n2.데이터 출력\n3.시합 시작");
 }
 
 function getTeamName(i){
@@ -25,25 +9,25 @@ function getTeamName(i){
   return teamName;
 }
 
-function inputError(j){
-  console.log(INPUT_GUIDE);
-  getMember(j);
-}
-
 function getMember(j){
   member = {};
   const memberData= prompt(`${j+1}번 타자 정보 입력`);
   if (!memberData.includes(",")){
-    inputError(j);
+    console.log("\n타자 정보는 \n'타자 이름, 타율'\n형식으로 입력해주세요.");
   } 
   name = memberData.split(',')[0];
   prob = Number(memberData.split(',')[1]);
   if (!(prob > 0.1 && prob < 0.5)) {
-    inputError(j);
+    console.log("\n타율은 0.1 초과 0.5 미만의 값으로 소수 셋째자리까지 입력해주세요.\n");
   }
   member.name = name;
   member.prob = prob;  
   return member;
+}
+
+function inputError(j){
+  console.log(INPUT_GUIDE);
+  getMember(j);
 }
 
 function inputTeamData(){
@@ -51,7 +35,7 @@ function inputTeamData(){
   for (let i=0; i<2; i++){
     members = [];
     teamName = getTeamName(i);
-    console.log(INPUT_GUIDE);
+    console.log("\n타자 정보는 \n'타자 이름, 타율'\n형식으로 입력해주세요.\n타율은 0.1 초과 0.5 미만의 값으로 소수 셋째자리까지 입력해주세요.\n");
     for (let j=0; j<9; j++){
       members.push(getMember(j));
     }
@@ -59,10 +43,9 @@ function inputTeamData(){
     team.name = teamName;
     team.members = members;
     teams.push(team);
-    console.log("");
   }
-  console.log("팀 데이터 입력이 완료되었습니다.\n");
-  selectMenu();
+  console.log("\n팀 데이터 입력이 완료되었습니다.\n");
+  main();
 }
 
 function showTeamData(){
@@ -73,10 +56,9 @@ function showTeamData(){
       member = team.members[j]
       console.log(`${j+1}번 ${member.name}, ${member.prob}`)
     }
-    console.log("");
   }
-  console.log("팀 데이터 출력이 완료되었습니다.\n");
-  selectMenu();
+  console.log("\n팀 데이터 출력이 완료되었습니다.\n");
+  main();
 }
 
 function selectMenu(){
@@ -93,27 +75,42 @@ function selectMenu(){
   }
 }
 
+function setup(){
+  const resultCount = {"스트라이크": 0, "볼": 0, "안타": 0, "아웃": 0};
+  for (let i=0; i<2; i++){
+    team = teams[i]
+    team.count = resultCount;
+    team.score = 0;
+    for (let j=0; j<9; j++){
+      member = team.members[j];
+      prob = member.prob;
+      member.weight = [];      
+      member.weight[0] = (1-prob)/2 - 0.05;
+      member.weight[1] = (1-prob)/2 - 0.05;
+      member.weight[2] = prob;
+      member.weight[3] = 0.1;
+    }
+  }
+  console.log(`${teams[0].name} VS ${teams[1].name}의 시합을 시작합니다!\n`);
+}
+
 function determineTurn(round){
   halfList = ["초", "말"];
   console.log(`${Math.floor(round/2)+1}회${halfList[round%2]} ${teams[round%2].name}의 공격\n`);
   return teams[round%2];
 }
 
-function setup(){
-  const resultCount = {"스트라이크": 0, "볼": 0, "안타": 0, "아웃": 0};
-  for (let i=0; i<2; i++){
-    teams[i].count = resultCount;
-    teams[i].score = 0;
-    for (let j=0; j<9; j++){
-      prob = teams[i].members[j].prob;
-      teams[i].members[j].weight = [];      
-      teams[i].members[j].weight[0] = (1-prob)/2 - 0.05;
-      teams[i].members[j].weight[1] = (1-prob)/2 - 0.05;
-      teams[i].members[j].weight[2] = prob;
-      teams[i].members[j].weight[3] = 0.1;
-    }
+function hitTheBall(hittingTeam, hitterNum){
+  hitter = hittingTeam.members[hitterNum];
+  console.log(`${hitterNum+1}번 ${hitter.name}`);
+  count = hittingTeam.count;
+  changeHitter = false;
+  while(changeHitter === false){
+    result = getResult(hitter.weight);
+    counting(count, result);
+    changeHitter = judge(count, result);
+    showCount(count);
   }
-  console.log(`${teams[0].name} VS ${teams[1].name}의 시합을 시작합니다!\n`);
 }
 
 function getResult(weight){
@@ -122,15 +119,15 @@ function getResult(weight){
     return "스트라이크";
   } else if (randNum<weight[0]+weight[1]){
     return "볼";
-  } else if (randNum<1-weight[3]){
+  } else if (randNum<weight[0]+weight[1]+weight[2]){
     return "안타";
   } else {
     return "아웃";
   }  
 }
 
-function showCount(count){
-  console.log(`${count["스트라이크"]}S ${count["볼"]}B ${count["아웃"]}O \n`) 
+function counting(count, result){
+  count[result] = count[result] + 1;
 }
 
 function judge(count, result){
@@ -150,20 +147,8 @@ function judge(count, result){
   }
 }
 
-function counting(count, result){
-  count[result] = count[result] + 1;
-}
-
-function hitTheBall(hittingTeam, hitterNum){
-  hitter = hittingTeam.members[hitterNum];
-  console.log(`${hitterNum+1}번 ${hitter.name}`);
-  changeHitter = false;
-  while(changeHitter === false){
-    result = getResult(hitter.weight);
-    counting(hittingTeam.count, result);
-    changeHitter = judge(hittingTeam.count, result);
-    showCount(hittingTeam.count);
-  }
+function showCount(count){
+  console.log(`${count["스트라이크"]}S ${count["볼"]}B ${count["아웃"]}O \n`);
 }
 
 function wrapUpGame(){
@@ -175,7 +160,7 @@ function wrapUpGame(){
 
 function gamePlaying(){
   setup();
-  for (let round=0; round<4; round++){
+  for (let round=0; round<12; round++){
     hittingTeam = determineTurn(round);
     for (let hitter=0; hittingTeam.count["아웃"]<3; hitter++){
       hitTheBall(hittingTeam, hitter%9);
@@ -190,7 +175,7 @@ function gamePlaying(){
 }
 
 function main(){
-  gameStart();
+  startingMessage();
   selectMenu();
 }
 
